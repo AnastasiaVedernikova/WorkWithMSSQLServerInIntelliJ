d95c66b117c0558a017093a18184fdf31372bc00
NB-CS2015-01\cs.ucu.edu.ua <vedernikova@ucu.edu.ua>
2017-04-21 12:01:26 +0300

    in

--- src/test/java/com/vedernikova/Test.java
+++ src/test/java/com/vedernikova/Test.java
@@ -9,26 +9,12 @@ public class Test {
     public static void main(String[] args) {
         try {
 
-
-            // Note :
-            // • SQL Server's port no can be found by using TCPView s/w
-            // OR
-            // • You can also set a fixed port for the server in Sql server TCP/IP
-            // properties
-            // • Sql Server's TCP/IP should be enabled first for this
-            // • A 'SQL' user should be created and GRANTED access to the Database
-            // • Rest is just as normal JDBC
-
             Class.forName("com.vedernikova.Test");
             System.out.println("# - Driver Loaded");
 
-
             String server = "localhost\\SOLEXPRESS";//Q?
             int port = 1433;
-//            String user = "paulo"; // Sql server username
-//            String password = "paulo";
             String database = "Parallel";
-
             String Url = "jdbc:sqlserver://"+server+":"+port+";databaseName="+database+";integratedSecurity=true";
           //jdbc:sqlserver://localhost\SOLEXPRESS:1433;databaseName=Parallel
            // System.out.print(Url);
@@ -36,17 +22,28 @@ public class Test {
             System.out.println("# - Connection Obtained");
 
             Statement stmt = con.createStatement();
+            Statement stmt1 = con.createStatement();
             System.out.println("# - Statement Created");
 
-            ResultSet rs = stmt.executeQuery("SELECT IDClient FROM Client;");
+
+            stmt1.executeUpdate("INSERT INTO MyDB " + "VALUES (2, '11/10/2016')");//додаєм інфу в табл
+
+            ResultSet rs = stmt.executeQuery("SELECT Data FROM MyDB;"); ///дістаєм інфу з таблиці
+//            stmt.executeQuery("DELETE FROM MyDB\n" +
+//                    "WHERE Data = '11/10/2016';");
+            String sql = "DELETE FROM MyDB " +            //видаляєм інфу з таблиці
+                    "WHERE ID = 1";
+            stmt1.executeUpdate(sql);
             System.out.println("# - Query Executed");
 
             if(rs.next()) {
-                System.out.println("Client Count : "+rs.getInt(1));
+                System.out.println("Client Count : "+rs.getString(1));
             }
+
 
             rs.close();
             stmt.close();
+            stmt1.close();
             con.close();
             System.out.println("# - Resources released");
         } catch (Exception ex) {
